fa96f3707aa3c68f266e5d29542f52872ae30a86
satyajitn <satyajit.nalavade@gmail.com>
2024-01-02 22:22:46 -0600

    added textloader and chunk text

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(text_splitter)
+
+print(docs)
+
