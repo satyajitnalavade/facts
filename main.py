43fdedebcfeb745751d1088b6b02a8e9a1e463ab
satyajitn <satyajit.nalavade@gmail.com>
2024-01-02 23:29:16 -0600

    created embedding

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -14,6 +18,14 @@ text_splitter = CharacterTextSplitter(
 
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter)
+
+# load it into Chroma
+db = Chroma.from_documents(
+    docs, 
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
 
 print(docs)
 
