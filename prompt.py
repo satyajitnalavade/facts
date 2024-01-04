51a5018071e5c6d45d71b782b5bbd1f6469803a5
satyajitn <satyajit.nalavade@gmail.com>
2024-01-04 00:26:33 -0600

    removing duplicate document with custom retriver

--- prompt.py
+++ prompt.py
@@ -1,20 +1,30 @@
-from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-
+from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 from dotenv import load_dotenv
+import langchain
+
+langchain.debug = True
 
 load_dotenv()
 
 chat = ChatOpenAI()
 embeddings = OpenAIEmbeddings()
+
 db = Chroma(
     persist_directory="emb",
     embedding_function=embeddings
 )
 
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(
+    embeddings=embeddings,
+    chroma=db
+)
+
+# for testing the duplication use regular retriver
+# retriever = db.as_retriever()
 
 chain = RetrievalQA.from_chain_type(
     llm=chat,
@@ -22,6 +32,6 @@ chain = RetrievalQA.from_chain_type(
     chain_type="stuff"
 )
 
-result = chain.run("Which bird has unique ability?")
+result = chain.run("What is an interesting fact about the English language?")
 
 print(result)
